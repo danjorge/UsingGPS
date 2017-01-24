dcbc13b1a4b06f3ac8d0f6142c685daacbecd928
unknown <daniel.souza@t46000011850.seplag.local>
2017-01-24 13:54:17 -0300

    Layout enhancement

--- app/src/main/java/danielsouza/com/usinggps/MainActivity.java
+++ app/src/main/java/danielsouza/com/usinggps/MainActivity.java
@@ -1,38 +1,25 @@
 package danielsouza.com.usinggps;
 
-import android.Manifest;
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.net.Uri;
-import android.os.Build;
-import android.provider.Settings;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
-
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-public class MainActivity extends Activity implements LocationListener {
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
     private Button botaoGerarLocalizacao;
     private TextView textoLocalizacaoGerada;
